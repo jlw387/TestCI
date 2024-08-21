c9fb520e596641510f46bdcef5a60fc1bb6f7e85
jlw387 <jlw387@cornell.edu>
2024-08-21 13:07:31 -0400

    Fix merge error
    Apparently the merge conflict results didn't save, and so the last commit still had the conflicts in the file. This commit fixes that.

--- main/main.py
+++ main/main.py
@@ -20,7 +20,6 @@ def zero(number):
     return 0
 
 if __name__ == "__main__":
-<<<<<<< HEAD
     print("Starting up program...\n")
 
     print("One plus one is:  ", add_one(1))
@@ -30,9 +29,3 @@ if __name__ == "__main__":
     
     print("\nThank you for running this program!")
     
-=======
-    print("One plus one is:", add_one(1))
-    print("Two times two is:", times_two(2))
-    print("Three cubed is:", cube(3))
-    print("Four zeroed is:", zero(4))
->>>>>>> development
