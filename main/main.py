e909c739f6f62ea4b8696f7c7c1f86384a2516d1
jlw387 <jlw387@cornell.edu>
2024-08-21 09:21:12 -0400

    Added cube function and relevant unit tests

--- main/main.py
+++ main/main.py
@@ -10,7 +10,13 @@ def times_two(number):
     
     return number * 2
 
+def cube(number):
+    if not (type(number) is float or type(number) is int):
+        raise TypeError("Must enter a number.")
+    
+    return number ** 3
+
 if __name__ == "__main__":
     print("One plus one is:", add_one(1))
     print("Two times two is:", times_two(2))
-    print("Development Branch")
+    print("Three cubed is:", cube(3))
